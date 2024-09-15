0c5eccf3ff72adc6fc7007019d67e67d76cd19f5
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-09-15 21:01:06 +1000

    lvim setup

---
+++ .config/lvim/config.lua
@@ -0,0 +1,84 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 };
+  { "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        window = {
+          width = 30,
+          mappings = {
+          ["p"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+          }
+        },
+        buffers = {
+          follow_current_file = true,
+        },
+        filesystem = {
+          follow_current_file = true,
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_by_name = {
+              "node_modules"
+            },
+            never_show = {
+              ".DS_Store",
+              "thumbs.db"
+            },
+          },
+        },
+      })
+    end
+  };
+  {"mfussenegger/nvim-dap-python"};
+}
+-- vim.cmd.colorscheme "catppuccin"
+lvim.colorscheme = "catppuccin-mocha"
+lvim.transparent_window = true
+
+-- Folding
+-- https://www.jackfranklin.co.uk/blog/code-folding-in-vim-neovim/
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+
+-- neo-tree
+lvim.builtin.nvimtree.active = false -- NOTE: using neo-tree
+lvim.builtin.which_key.mappings["e"] = {
+  ":Neotree toggle<CR>", "side file tree"
+}
+lvim.builtin.which_key.mappings["<s-e>"] = {
+  ":Neotree position=float toggle<CR>", "full file tree"
+}
+
+--NOTES FOR FUTURE ME
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+-- https://github.com/3rd/image.nvim?tab=readme-ov-file#installing-imagemagick
+-- https://github.com/leafo/magick
+
+-- Python stuff
+-- automatically install python syntax highlighting
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+}
+
+-- setup debug adapter
+-- lvim.builtin.dap.active = true
+-- local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+-- pcall(function()
+--   require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+-- end)
