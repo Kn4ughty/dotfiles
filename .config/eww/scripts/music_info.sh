e61e523803cf48d454bd61b8741fdf12dd3e9054
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-05-23 23:46:04 +1000

    Fix up eww play bar for music

--- .config/eww/scripts/music_info.sh
+++ .config/eww/scripts/music_info.sh
@@ -5,41 +5,27 @@ STATUS="$(mpc status)"
 COVER="/tmp/.music_cover.jpg"
 MUSIC_DIR="$HOME/Music"
 
-## Get status
-get_status() {
-	if [[ $STATUS == *"[playing]"* ]]; then
-		echo "images/icons/music/pause-button.png"
-	else
-		echo "images/icons/music/play-button.png"
-	fi
+get_status_image() {
+    playerctl --follow status 2>/dev/null | while read -r status; do
+        if [[ "$status" == "Playing" ]]; then
+            echo "images/music/pause-button.png"
+        else
+            echo "images/music/play-button.png"
+        fi
+    done
 }
 
-## Get song
-get_song() {
-    playerctl --follow metadata --format {{title}}
+get_status_glyph() {
+    playerctl --follow status 2>/dev/null | while read -r status; do
+        if [[ "$status" == "Playing" ]]; then
+            echo ""
+        else
+            echo ""
+        fi
+    done
 }
 
-## Get artist
-get_artist() {
-    playerctl --follow metadata --format {{artist}}
-}
-
-get_time() {
-    playerctl --follow metadata --format '{{duration(position)}}'
-}
-
-get_time_s() {
-    playerctl --follow metadata --format '{{position}}'
-}
-
-get_total_time() {
-    playerctl --follow metadata --format '{{duration(mpris:length)}}'
-}
-
-get_total_time_s() {
-    playerctl --follow metadata --format '{{mpris:length}}'
-}
-
+# Im not using this ATM but keeping it for future reference
 ## Get cover
 get_cover() {
 	ffmpeg -i "${MUSIC_DIR}/$(mpc current -f %file%)" "${COVER}" -y &> /dev/null
@@ -54,22 +40,10 @@ get_cover() {
 }
 
 ## Execute accordingly
-if [[ "$1" == "--song" ]]; then
-	get_song
-elif [[ "$1" == "--artist" ]]; then
-	get_artist
-elif [[ "$1" == "--status" ]]; then
-	get_status
-elif [[ "$1" == "--time" ]]; then
-	get_time
-elif [[ "$1" == "--time_s" ]]; then
-	get_time_s
-elif [[ "$1" == "--totaltime" ]]; then
-	get_total_time
-elif [[ "$1" == "--totaltime_s" ]]; then
-	get_total_time_s
-elif [[ "$1" == "--cover" ]]; then
-	get_cover
+if [[ "$1" == "--status" ]]; then
+	get_status_image
+elif [[ "$1" == "--statusg" ]]; then
+	get_status_glyph
 elif [[ "$1" == "--toggle" ]]; then
 	mpc -q toggle
 elif [[ "$1" == "--next" ]]; then
