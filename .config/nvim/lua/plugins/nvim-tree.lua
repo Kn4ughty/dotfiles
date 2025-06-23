86287d8dad173c49931c2197055d8d85c29d9b69
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-06-23 13:49:09 +1000

    Nvim image support

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -24,6 +24,7 @@ return {
             filesystem = {
                 filtered_items = {
                     hide_dotfiles = false,
+                    hide_gitignored = false,
                 },
                 always_show = {
                     ".",
