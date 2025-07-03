53d2eb31e079ea088ed82bce3dc4dd937bc63f51
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-07-03 14:43:13 +1000

    Add network module to bar

---
+++ .config/eww/scripts/network.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# output of `nmcli -m multiline -t -f STATE`
+# STATE:connected
+# CONNECTIVITY:full
+# WIFI-HW:enabled
+# WIFI:enabled
+# WWAN-HW:missing
+# WWAN:enabled
+# METERED:no (guessed)
+
+# Get value with `nmcli -g KEY g` -> value
+
+# For STATE,        it is "connected"   | "disconnected"
+# For CONNECTIVITY, it is "full"        | "none"
+
+DEVICE_NAME=$(nmcli -g DEVICE device status | head -n 1 )
+
+get_wifi_icon () {
+    local state
+    state=$1
+
+    case "$STATE" in
+        "connecting")
+            echo 󱛇
+            return 0
+            ;;
+        "connected")
+            echo 
+            return 0
+            ;;
+        "disconnecting")
+            echo 󱚼
+            return 0
+            ;;
+        "disconnected")
+            echo 󰤯
+            return 0
+            ;;
+        *)
+            echo 󰤫
+            return 1
+            ;;
+    esac
+}
+
+nmcli m | 
+while read -r line; do
+    DATA=$(nmcli -m multiline -t g)
+    STATE=$(rg 'STATE:(.+)' -or '$1' <<< $DATA)
+    IP=$(nmcli -g IP4.ADDRESS device show $DEVICE_NAME)
+    # WIFI_HW=$(rg 'WIFI-HW:(.+)' -or '$1' <<< $DATA)
+    # DEVICE_STATUS=$(nmcli -m multiline -t device status)
+
+    icon=$(get_wifi_icon "${STATE}")
+
+    jq -c -n \
+        --arg state "${STATE}"\
+        --arg icon "${icon}"\
+        --arg ip "${IP}"\
+        '{ state: $state, icon: $icon, ip: $ip}'
+done
