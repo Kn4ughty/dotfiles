ff67394b7bdfb4e1924896f756c7268dad93dd08
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-07-03 13:53:38 +1000

    Add icons to battery script

--- .config/eww/scripts/battery.sh
+++ .config/eww/scripts/battery.sh
@@ -1,25 +1,152 @@
 #!/bin/bash
-quick=$(acpi -b)
-percentage=$(rg '([0-9]+%)' -or '$1' <<< $quick)
-time_empty=$(rg ', (([0-9]+:)+[0-9]+)' -or '$1' <<< $quick)
+# this script isnt very good
 
-echo '{ "percent": "'$percentage'", "time": "'$time_empty '"}'
+# quick=$(acpi -b)
+# percentage=$(rg '([0-9]+%)' -or '$1' <<< $quick)
+# time_display=$(rg ', (([0-9]+:)+[0-9]+)' -or '$1' <<< $quick)
+#
+# echo '{ "percent": "'$percentage'", "time": "'$time_display '"}'
+
+
+get_icon () {
+    local percentage 
+    local ischarging
+
+    percentage=$1
+    ischarging=$2
+
+    if [[ -z ischarging ]]; then
+        echo 󱉝
+        return 0
+    fi
+
+    if [[ -n $ischarging ]]; then
+        if (( $percentage == 100 )); then
+            echo 󰂅
+            return 0
+        fi
+        if (( $percentage >= 90 )); then
+            echo 󰂋
+            return 0
+        fi
+        if (( $percentage >= 80 )); then
+            echo 󰂊
+            return 0
+        fi
+        if (( $percentage >= 70 )); then
+            echo 󰢞
+            return 0
+        fi
+        if (( $percentage >= 60 )); then
+            echo 󰂉
+            return 0
+        fi
+        if (( $percentage >= 50 )); then
+            echo 󰢝
+            return 0
+        # ...
+        fi
+        if (( $percentage >= 40 )); then
+            echo 󰂈
+            return 0
+        fi
+        if (( $percentage >= 30 )); then
+            echo 󰂇
+            return 0
+        fi
+        if (( $percentage >= 20 )); then
+            echo 󰂆
+            return 0
+        fi
+        if (( $percentage >= 10 )); then
+            echo 󰢜
+            return 0
+        fi
+        echo 󰢟
+        return 0
+    else
+        if (( $percentage == 100 )); then
+            echo 󰁹
+            return 0
+        fi
+        if (( $percentage >= 90 )); then
+            echo 󰂂
+            return 0
+        fi
+        if (( $percentage >= 80 )); then
+            echo 󰂁
+            return 0
+        fi
+        if (( $percentage >= 70 )); then
+            echo 󰂀
+            return 0
+        fi
+        if (( $percentage >= 60 )); then
+            echo 󰁿
+            return 0
+        fi
+        if (( $percentage >= 50 )); then
+            echo 󰁾
+            return 0
+        fi
+        if (( $percentage >= 40 )); then
+            echo 󰁽
+            return 0
+        fi
+        if (( $percentage >= 30 )); then
+            echo 󰁼
+            return 0
+        fi
+        if (( $percentage >= 20 )); then
+            echo 󰁻
+            return 0
+        fi
+        if (( $percentage >= 10 )); then
+            echo 󰁺
+            return 0
+        fi
+        echo 󰂎
+        return 0
+    fi
+}
+
+# get_icon "12" 'yes'
+
 
 upower --monitor-detail -i $(upower -e | grep 'bat') |
 while read -r line; do
-    if [[ $line =~ ^native-path: ]]; then
-        if [[ -n $buffer ]]; then
-            # echo $buffer
-            percentage=$(rg 'percentage: +(.+%)' -or '$1' <<< $buffer)
-            # echo $percentage
-            time_empty=$(rg 'time to empty: +(.+)' -or '$1' <<< $buffer)
+    if [[ $line =~ ^native-path: ]] && [[ -n $buffer ]]; then
 
-            echo '{ "percent": "'$percentage'", "time": "'$time_empty '"}'
-
-            buffer=""
+        percentage=$(rg 'percentage: +([0-9]+)' -or '$1' <<< $buffer)
+        if [[ -z $percentage ]]; then
+            percentage=1
         fi
+        state=$(rg 'state: +(.+)' -or '$1' <<< $buffer)
+
+        if [[ -z $state ]]; then
+            state='unknown'
+        fi
+        
+        
+        if [[ $state == "charging" ]]; then
+            time_display=$(rg 'time to full: +(.+)' -or '$1' <<< $buffer)
+            is_charging="yes"
+        else
+            ischarging=''
+            time_display=$(rg 'time to empty: +(.+)' -or '$1' <<< $buffer)
+        fi
+        
+        # Get icon
+        icon=$(get_icon $percentage $is_charging)
+
+
+
+        echo '{ "percent": "'$icon $percentage'%", "time": "'$time_display '", "icon": "'$icon'"}'
+
+        buffer=""
     fi
 
     buffer+="$line"$'\n'
 done
+
 
