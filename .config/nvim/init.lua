e4c35a7da9e945d8c2ba3ac33d3c994e6aacf05f
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-11-01 17:29:29 +1100

    cargo clippy to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -186,6 +186,18 @@ vim.pack.add({
     { src = 'https://github.com/neovim/nvim-lspconfig' },
 })
 
+vim.lsp.config('rust_analyzer', {
+    settings = {
+        ["rust-analyzer"] = {
+            check = {
+                command = "clippy"
+            },
+        }
+    }
+})
+
+
+
 -- -- Mason
 vim.pack.add({
     { src = 'https://github.com/mason-org/mason.nvim' },
