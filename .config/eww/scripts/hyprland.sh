10c504e35cc493617b3af3f479c46f1633477f5c
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-01-04 18:35:16 +1100

    finish fixing eww hyprland bar

--- .config/eww/scripts/hyprland.sh
+++ .config/eww/scripts/hyprland.sh
@@ -12,7 +12,9 @@ handle() {
 output() {
     case $arg1 in 
         "workspace")
-            hyprctl workspaces -j | jq -c
+            active=$(hyprctl activeworkspace -j | jq ".id")
+            # hyprctl workspaces -j | jq -c --arg active "$active" ''
+            hyprctl workspaces -j | jq -c --argjson active "$active" 'map(.+={"focused":.id==$active})'
             ;;
         "title")
             hyprctl activewindow -j | jq ".title" 
@@ -24,7 +26,12 @@ output() {
 
 arg1=$1
 
+# if [ $2 = "initial" ]; then
+#     output
+#     exit
+# fi
 output
+
 
 socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock \
     | while read -r line; do handle "$line"; done
