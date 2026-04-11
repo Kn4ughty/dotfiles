0287f8557f064207c9bb4f347fa21dd743d0b56a
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-04-11 14:56:56 +1000

    add yuck syntax highlighting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -270,11 +270,48 @@ vim.pack.add({
 })
 require("mason-lspconfig").setup()
 
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'yuck' },
-    callback = function() vim.treesitter.start() end,
+-- Treesitter
+vim.pack.add({
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+    { src = 'https://github.com/elkowar/yuck.vim' },
 })
+require('nvim-treesitter').setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "yuck",
+        "markdown_inline" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = { "javascript" },
+
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+    highlight = {
+        enable = true,
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+-- vim.api.nvim_create_autocmd('FileType', {
+--     pattern = { 'yuck' },
+--     callback = function() vim.treesitter.start() end,
+-- })
 
 
 -- Dropbar
