b844d8f0b929cefc8fb4f17b0ded917fc8dd5dfb
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-07-29 16:37:11 +1000

    small luakit change

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -1,2 +1,22 @@
+--[[
+# Keybinds/Commands
+":tabopen URL"
+"o" switch to command mode with open already typed
+    if path is not url, then opens in search bar
+"O" open with current url
+
+"d" close tab
+"u" undo
+"B" Bookmark current page
+"gb" goto bookmarks
+"f" easymotion links
+--]]
+
 require "vertical_tabs"
 -- require "usertheme.lua"
+
+
+local engines = settings.window.search_engines
+engines.duck         = "https://duckduckgo.com/?q=%s"
+engines.aur          = "https://aur.archlinux.org/packages?K=%s"
+engines.default = engines.duck
